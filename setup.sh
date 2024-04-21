16a74a058b3b2f3854c5b2fcf7bea06b5dbe9238
MiddleDistances <64740417+MiddleDistances@users.noreply.github.com>
2024-04-21 11:29:48 +0800

    Update setup.sh
    full script

--- setup.sh
+++ setup.sh
@@ -79,3 +79,62 @@ else
 fi
 
 echo "Setup complete! Your device $selected_part is set up at $mount_dir."
+
+
+# Get the current user
+current_user=$(logname)
+# Download the GitHub repository
+echo "Downloading program files from GitHub..."
+sudo apt-get install -y git
+sudo rm -rf /home/$current_user/flydownloader  # Remove the existing directory and its contents
+sudo git clone https://github.com/MiddleDistances/flydownloader.git /home/$current_user/flydownloader
+# Create a helper file with the mass storage directory and username
+echo "mount_dir=$mount_dir" | sudo -u "$current_user" tee "/home/$current_user/flydownloader/storage_path.txt"
+echo "username=$current_user" | sudo -u "$current_user" tee -a "/home/$current_user/flydownloader/storage_path.txt"
+# Change ownership of the flydownloader directory to the current user
+echo "Changing ownership of the flydownloader directory..."
+sudo chown -R $current_user:$current_user /home/$current_user/flydownloader
+# Setup Python environment and install dependencies
+echo "Setting up Python environment..."
+sudo apt-get install -y python3-venv
+python3 -m venv /home/$current_user/flydownloader/venv
+source /home/$current_user/flydownloader/venv/bin/activate
+pip install -r /home/$current_user/flydownloader/requirements.txt
+deactivate
+#install samba
+sudo apt-get install samba -y
+sudo systemctl restart smbd
+# Configure Samba to share the mounted device
+echo "Configuring Samba..."
+sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.backup
+echo "\[global\]
+workgroup = WORKGROUP
+server string = Samba Server
+security = user
+create mask = 0664
+directory mask = 0775
+\[$device_name\]
+path = $mount_dir
+writeable = Yes
+create mask = 0664
+directory mask = 0775
+public = no
+valid users = $current_user" | sudo tee -a /etc/samba/smb.conf
+sudo systemctl restart smbd
+# Create a systemd service to run the script at boot
+echo 'Creating systemd service...1'
+sudo tee /etc/systemd/system/flydownloader.service <<EOF
+[Unit]
+Description=Fly Downloader Service
+After=network.target
+[Service]
+ExecStart=/home/pi/flydownloader/venv/bin/python /home/pi/flydownloader/USB_connect_download_v2.py
+WorkingDirectory=/home/pi/flydownloader
+Restart=always
+[Install]
+WantedBy=multi-user.target
+EOF
+# Enable and start the service
+sudo systemctl enable flydownloader.service
+sudo systemctl start flydownloader.service
+echo "Setup complete!"
